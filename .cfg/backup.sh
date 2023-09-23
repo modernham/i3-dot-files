c9550226ee100a5a0c8adfc1bd3f8489e5f1e165
OpcodeOperator <billy@penley.io>
2023-09-23 17:18:06 -0500

    Updated DotFiles

--- .cfg/backup.sh
+++ .cfg/backup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 alias config='/usr/bin/git --git-dir=/home/opcode/.cfg/ --work-tree=/home/opcode'
 cd /home/opcode
-config commit -m "Updated DotFiles"
+/usr/bin/git --git-dir=/home/opcode/.cfg/ --work-tree=/home/opcode add -u :/
 /usr/bin/git --git-dir=/home/opcode/.cfg/ --work-tree=/home/opcode commit -m "Updated DotFiles"
 cd /home/opcode/.cfg
 /usr/bin/git --git-dir=/home/opcode/.cfg/ --work-tree=/home/opcode push
-config push
